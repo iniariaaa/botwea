d08cb6fc781bcd9d0fe4f91d27015bb6d3b82302
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-08-09 16:28:36 +0700

    Update cersex.js

--- plugins/cersex.js
+++ plugins/cersex.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
 
   let res = await fetch('https://ariaputrapratama.herokuapp.com/api/cersex')
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
   if (json.status != 200) throw json
-  conn.sendFile(m.chat, json.result.img, 'eror.jpg', `*Judul:* ${json.result.judul}\n*Cerita:* ${json.result.cersex}`, m, false, { thumbnail: await (await fetch(json.data.img)).buffer() })
+  conn.sendFile(m.chat, json.result.img, 'eror.jpg', `*Judul:* ${json.result.judul}\n*Cerita:* ${json.result.cersex}`, m, false, { thumbnail: await (await fetch(json.result.img)).buffer() })
 }
 handler.help = ['cersex']
 handler.tags = ['tools']
