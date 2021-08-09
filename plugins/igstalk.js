2f967c93d0738ec2700c2ac33124fbc07056c2a1
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-08-09 16:23:53 +0700

    Update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,15 +1,14 @@
 const fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `contoh:\n${usedPrefix + command} AriaBotz`
 
-  let res = await fetch(global.API('zekais', '/igs', { username: args[0] }))
+  let res = await fetch('https://ariaputrapratama.herokuapp.com/api/cersex')
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
   if (json.status != 200) throw json
-  conn.sendFile(m.chat, json.data.profilehd, 'eror.jpg', `*Nama:* ${json.data.fullname}\n*Bio:* ${json.data.bio}\n*Followers:* ${json.data.follower}\n*Following:* ${json.data.following}\n*Posts:* ${json.data.timeline}\n*Private:* ${json.data.private ? 'Ya' : 'Tidak'}`, m, false, { thumbnail: await (await fetch(json.data.profilehd)).buffer() })
+  conn.sendFile(m.chat, json.data.img, 'eror.jpg', `*Judul:* ${json.result.judul}\n*Cerita:* ${json.result.cersex}`, m, false, { thumbnail: await (await fetch(json.data.img)).buffer() })
 }
-handler.help = ['igstalk <username>']
+handler.help = ['cersex']
 handler.tags = ['tools']
-handler.command = /^(igstalk)$/i
+handler.command = /^(cersex)$/i
 handler.limit = true
 module.exports = handler
