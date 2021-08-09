55f27c493bfe11e2b3c414ddb93ba23d426dd87d
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-08-09 15:09:17 +0700

    Create sirine.js

---
+++ plugins/sirine.js
@@ -0,0 +1,11 @@
+let fs = require('fs')
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let zero = fs.readFileSync('./audio/sirine.mp3')
+conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
+}
+
+handler.customPrefix = /^(door)?$/i
+handler.command = new RegExp
+
+module.exports = handler
