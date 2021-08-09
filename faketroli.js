28a5084868aaef46d3bd76940fbb4b4688b1b5b2
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-08-09 17:20:36 +0700

    Create faketroli.js

---
+++ faketroli.js
@@ -0,0 +1,43 @@
+const { MessageType } = require("@adiwajshing/baileys")
+let axios = require("axios");
+let fs = require("fs");
+let handler = async(m, { conn, args, text, usedPrefix: _p }) => {
+let pp = './src/avatar_contact.png'
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+//if(args.length == 0) return m.reply(`Cara Penggunaan: Reply Foto, Ketik *${_p}faketroli* <TextMessage> | <OrderMessage> | <OrderTitle>\nContoh: *${_p}faketroli* Hai Wibu | SGDC-BOT | 10k Bang?`) 
+	let [poop, tytyd] = text.split("|")
+	if(!poop) return m.reply("Masukkan Teks Utama")
+	if(!tytyd) tytyd = "Cloud Bot"
+	//if(!kecil) return m.reply("Masukkan Teks Title Order!")
+  try{
+//let q = m.quoted ? m.quoted : m
+//let mime = (q.msg || q).mimetype || ''
+   // if (!/image\/(jpe?g|png)/.test(mime)) return m.reply("Foto Aja Om :)")
+    //let img = await q.download()
+    pp = await conn.getProfilePicture(who)
+	const xixi = {
+	                  key : {
+                                participant : '0@s.whatsapp.net'
+                               },
+                    message: {
+                    orderMessage: {
+                            itemCount : 1,
+                            status: 1,
+                            surface : 1,
+                            message: Gans,
+                            orderTitle: "AriaBotz",
+                            thumbnail: pp,
+                            sellerJid: '0@s.whatsapp.net'
+                        }
+                }
+          }
+          conn.sendMessage(m.chat, poop, MessageType.extendedText, { quoted: xixi })
+      }catch (e){
+      	m.reply("```ERROR```")
+      console.log(e)
+      }
+}
+
+handler.command = /^(faketroli)$/i
+
+module.exports = handler
