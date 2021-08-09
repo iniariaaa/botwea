5c26ecc2c77e83e1edfbb3f47bd94e6f9786c573
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-08-09 16:24:30 +0700

    Update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,14 +1,1 @@
-const fetch = require('node-fetch')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  let res = await fetch('https://ariaputrapratama.herokuapp.com/api/cersex')
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
-  let json = await res.json()
-  if (json.status != 200) throw json
-  conn.sendFile(m.chat, json.data.img, 'eror.jpg', `*Judul:* ${json.result.judul}\n*Cerita:* ${json.result.cersex}`, m, false, { thumbnail: await (await fetch(json.data.img)).buffer() })
-}
-handler.help = ['cersex']
-handler.tags = ['tools']
-handler.command = /^(cersex)$/i
-handler.limit = true
-module.exports = handler
