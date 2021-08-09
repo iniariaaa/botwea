380f383ac0230ff10e1af16ce63bb9999db3493e
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-08-09 23:09:14 +0700

    Update casino.js

--- plugins/casino.js
+++ plugins/casino.js
@@ -5,27 +5,27 @@ let handler = async (m, { conn, args, usedPrefix, DevMode }) => {
     if (m.chat in conn.casino) return m.reply ('Masih ada yang melakukan casino disini, tunggu sampai selesai!!')
     else conn.casino[m.chat] = true
     try {
-        let randombot = `${Math.floor(Math.random() * 101)}`.trim()
-        let randomkamu = `${Math.floor(Math.random() * 101)}`.trim()
-        let Bot = (randomaku * 1)
+        let randomaku = `${Math.floor(Math.random() * 101)}`.trim()
+        let randomkamu = `${Math.floor(Math.random() * 81)}`.trim() //hehe Biar Susah Menang :v
+        let Aku = (randomaku * 1)
         let Kamu = (randomkamu * 1)
         let count = args[0]
-        count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].money / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
+        count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
         count = Math.max(1, count)
         if (args.length < 1) return conn.reply(m.chat, usedPrefix + 'casino <jumlah>\n ' + usedPrefix + 'casino 1000', m)
-        if (global.db.data.users[m.sender].money >= count * 1) {
-            global.db.data.users[m.sender].money -= count * 1
+        if (global.db.data.users[m.sender].exp >= count * 1) {
+            global.db.data.users[m.sender].exp -= count * 1
             //await m.reply('') //Kwkwwkkwlwlw
-            if (Bot > Kamu) {
-                conn.reply(m.chat, `💰 Casino 💰\n*Kamu:* ${Kamu} Point\n*Computer:* ${Bot} Point\n\n*You LOSE*\nKamu kehilangan ${count} Uang(money)`.trim(), m)
-            } else if (Bot < Kamu) {
-                global.db.data.users[m.sender].money += count * 2
-                conn.reply(m.chat, `💰 Casino 💰\n*Kamu:* ${Kamu} Point\n*Computer:* ${Bot} Point\n\n*You Win*\nKamu mendapatkan ${count * 2} Uang(money)`.trim(), m)
+            if (Aku > Kamu) {
+                conn.reply(m.chat, `ðŸ’° Casino ðŸ’°\n*Kamu:* ${Kamu} Point\n*Computer:* ${Aku} Point\n\n*You LOSE*\nKamu kehilangan ${count} Uang(xp)`.trim(), m)
+            } else if (Aku < Kamu) {
+                global.db.data.users[m.sender].exp += count * 2
+                conn.reply(m.chat, `ðŸ’° Casino ðŸ’°\n*Kamu:* ${Kamu} Point\n*Computer:* ${Aku} Point\n\n*You Win*\nKamu mendapatkan ${count * 2} Uang(xp)`.trim(), m)
             } else {
-                global.db.data.users[m.sender].money += count * 1
-                conn.reply(m.chat, `💰 Casino 💰\n*Kamu:* ${Kamu} Point\n*Computer:* ${Bot} Point\n\n*SERI*\nKamu mendapatkan ${count * 1} Uang(money)`.trim(), m)
+                global.db.data.users[m.sender].exp += count * 1
+                conn.reply(m.chat, `ðŸ’° Casino ðŸ’°\n*Kamu:* ${Kamu} Point\n*Computer:* ${Aku} Point\n\n*SERI*\nKamu mendapatkan ${count * 1} Uang(xp)`.trim(), m)
             }
-        } else conn.reply(m.chat, `Uang(money) kamu tidak mencukupi untuk Casino silahkan */kerja* terlebih dahulu!`.trim(), m)
+        } else conn.reply(m.chat, `Uang(xp) kamu tidak mencukupi untuk Casino silahkan *#kerja* terlebih dahulu!`.trim(), m)
     } catch (e) {
         console.log(e)
         m.reply('Error!!')
