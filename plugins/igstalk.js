512d2dc148a2b89f466462c3c6a0e0cc7f284b71
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-08-09 16:33:33 +0700

    Update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (json.status != 200) throw json
-  conn.sendFile(m.chat, json.data.profilehd, 'eror.jpg', `*Nama:* ${json.data.fullname}\n*Bio:* ${json.data.bio}\n*Followers:* ${json.data.follower}\n*Following:* ${json.data.following}\n*Posts:* ${json.data.timeline}\n*Private:* ${json.data.private ? 'Ya' : 'Tidak'}\n\nhttp://instagram.com/stikerinbot`, m, false, { thumbnail: await (await fetch(json.data.profilehd)).buffer() })
+  conn.sendFile(m.chat, json.data.profilehd, 'eror.jpg', `*Nama:* ${json.data.fullname}\n*Bio:* ${json.data.bio}\n*Followers:* ${json.data.follower}\n*Following:* ${json.data.following}\n*Posts:* ${json.data.timeline}\n*Private:* ${json.data.private ? 'Ya' : 'Tidak'}`, m, false, { thumbnail: await (await fetch(json.data.profilehd)).buffer() })
 }
 handler.help = ['igstalk <username>']
 handler.tags = ['tools']
