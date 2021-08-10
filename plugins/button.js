47b59b44a6cd21a5466d31605494147ddcde0bc6
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-08-10 07:18:02 +0700

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) =>
 conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
                     "listMessage":  {
                         "title": "*List Menu AriaBotzz*",
-                        "description":"Silahkan Pilih List Menu Dibawah\nBot Aktif Dibatas Thanks",
+                        "description":"Silahkan Pilih List Menu Dibawah\nBot Aktif Dibatas Thanks\nBagi Yang Mau Join Silahkan Klik Link Dibawah\nhttps://chat.whatsapp.com/IdK7gOMQQpC7WdfIgtuQtn",
                         "buttonText": "Menu disini",
                         "listType": "SINGLE_SELECT",
                         "sections": [
