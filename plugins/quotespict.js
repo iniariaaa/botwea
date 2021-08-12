ba74db387b37994b9754d476367ca66dbb5bc227
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-08-12 21:32:05 +0700

    Create quotespict.js

---
+++ plugins/quotespict.js
@@ -0,0 +1,15 @@
+// xie hua piao piao batman membuka kulkas dan mngambil semangka lalu memberikan mayo di atas semangka
+
+let fetch = require('node-fetch')
+let handler = async(m, { conn }) => {
+  let res = await fetch('http://zekais-api.herokuapp.com/quotepic')
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.result, '', 'Nih Quotesnya', m)
+}
+handler.help = ['quotespict']
+handler.tags = ['quotes']
+handler.command = /^picts$/i
+
+module.exports = handler
