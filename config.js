0a7e46cfd3725f9e7465d5f3f43db4e468f45f8b
ZEROBOT <88374559+ZeroChanBot@users.noreply.github.com>
2021-08-24 15:42:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'HIRO',
+  'https://api.xteam.xyz': 'beli',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'AriaGanzTzy',
 }
