5c542d79d8cf84f830bf7671ee3e0050b20522d5
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-16 14:20:46 +0700

    Update poke.js

--- plugins/poke.js
+++ plugins/poke.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn }) => {
   let res = await fetch('https://ariarestapii.herokuapp.com/api/poke?apikey=aria')
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.gif) throw 'Error!'
-  conn.sendFile(m.chat, json.gif, '', 'Dih Sange Sama Anime', m)
+  if (!json.image) throw 'Error!'
+  conn.sendFile(m.chat, json.image, '', 'Dih Sange Sama Anime', m)
 }
 handler.help = ['poke']
 handler.command = /^poke$/i
