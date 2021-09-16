2b1112fb8bacb56678cca00f403cc5dcf16d1d2f
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-16 14:08:33 +0700

    Create poke.js

---
+++ plugins/poke.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn }) => {
+  let res = await fetch('https://ariarestapii.herokuapp.com/api/poke?apikey=aria')
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.gif, '', 'Dih Sange Sama Anime', m)
+}
+handler.help = ['poke']
+handler.command = /^poke$/i
+
+module.exports = handler
