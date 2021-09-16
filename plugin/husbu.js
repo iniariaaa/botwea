c17eea747ae7e186cf43f2535379c2715db0d996
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-16 14:23:44 +0700

    Update and rename plugins/poke.js to plugin/husbu.js

--- plugin/husbu.js
+++ plugin/husbu.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
-  let res = await fetch('https://ariarestapii.herokuapp.com/api/poke?apikey=aria')
+  let res = await fetch('https://ariarestapii.herokuapp.com/api/husbu?apikey=aria')
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.image) throw 'Error!'
-  conn.sendFile(m.chat, json.image, '', 'Dih Sange Sama Anime', m)
+  conn.sendFile(m.chat, json.image, '', 'Ganteng Kan Kek Gua', m)
 }
 handler.help = ['poke']
 handler.command = /^poke$/i
