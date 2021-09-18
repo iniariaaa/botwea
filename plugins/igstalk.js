1cd563cec59b1df0caca4c1b6e9ecd36da14a206
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-18 13:57:43 +0700

    Update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,36 +1,15 @@
-let imageToBase64 = require('image-to-base64');
-let axios = require("axios");
-let handler = async(m, { conn, text }) => {
+const fetch = require('node-fetch')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `contoh:\n${usedPrefix + command} ini.aria`
 
-    if (!text) return conn.reply(m.chat, 'Masukan Username yang akan distalk', m)
-
-  await m.reply('Searching...')
-    axios.get('https://ariarestapii.herokuapp.com/api/ig/stalk?username=${text}&apikey=aria')
-    .then((res) => {
-      imageToBase64(res.data.profile)
-        .then(
-          (ress) => {
-            let buf = Buffer.from(ress, 'base64')
-            let hasil = `*IG STALKER*\n\nFullName : ${res.data.fullname}\nFollowers : ${res.data.follower}\nFollowing : ${res.data.following}\nBio : ${res.data.bio}`
-
-    conn.reply(m.chat, buf, 'foto.jpg', hasil, m)
-         })
-	})
+  let res = await fetch(global.API('aria', '/api/ig/stalk', { username: args[0] }))
+  if (!res.ok) throw eror
+  let json = await res.json()
+  if (json.status != 200) throw json
+  conn.sendFile(m.chat, json.data.profilehd, 'eror.jpg', `*Nama:* ${json.data.fullname}\n*Bio:* ${json.data.bio}\n*Followers:* ${json.data.follower}\n*Following:* ${json.data.following}\n*Posts:* ${json.data.timeline}\n*Private:* ${json.data.private ? 'Ya' : 'Tidak'}`, m, 0, { thumbnail: await (await fetch(json.data.profilehd)).buffer() })
 }
-handler.help = ['igstalk','stalkig'].map(v => v + ' <@user>')
-handler.tags = ['internet']
-handler.command = /^(igstalk|stalkig)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
+handler.help = ['igstalk <username>']
+handler.tags = ['tools']
+handler.command = /^(igstalk)$/i
 handler.limit = true
-
 module.exports = handler
