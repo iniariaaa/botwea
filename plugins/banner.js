247933aa8675c6866c6ebdacbf8270992162f879
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-19 13:36:03 +0700

    Create banner.js

---
+++ plugins/banner.js
@@ -0,0 +1,25 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args[0]) throw 'Masukkan Parameter Teks'
+  m.reply('Sedang Diproses...')
+  let res = `https://ariarestapii.herokuapp.com/api/oxy/banner?text=${response}&apikey=aria`
+  conn.sendFile(m.chat, res, 'banner.jpg', `Ini Kak`, m, false)
+}
+handler.help = ['banner'].map(v => v + ' <teks>')
+handler.tags = ['photooxy']
+
+handler.command = /^(banner)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = true
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
