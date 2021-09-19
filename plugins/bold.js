fece69788130a00e637ce4072fa65455ca576126
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-19 12:45:11 +0700

    Create bold.js

---
+++ plugins/bold.js
@@ -0,0 +1,25 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args[0]) throw 'Masukkan Parameter Teks'
+  m.reply('Sedang Diproses...')
+  let res = `https://ariarestapii.herokuapp.com/api/oxy/bold?text=${response}&apikey=aria`
+  conn.sendFile(m.chat, res, 'poly.mp4', `Ini Kak`, m, false)
+}
+handler.help = ['poly'].map(v => v + ' <teks>')
+handler.tags = ['image']
+
+handler.command = /^(poly)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = true
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
