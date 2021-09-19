16184b305743970774e2e377adfb547ad314fc9c
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-19 11:31:01 +0700

    Update poly.js

--- plugins/poly.js
+++ plugins/poly.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ')
-  if (!args[0]) throw 'Masukkan Parameter'
+  if (!args[0]) throw 'Masukkan Parameter Teks'
   m.reply('Sedang Diproses...')
   let res = `https://ariarestapii.herokuapp.com/api/oxy/poly?text=${response}&apikey=aria`
   conn.sendFile(m.chat, res, 'poly.mp4', `Ini Kak`, m, false)
