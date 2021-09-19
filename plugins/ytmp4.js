e66f20f1e5194882011c6e65e3e303f86aa20234
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-19 17:58:00 +0700

    Update ytmp4.js

--- plugins/ytmp4.js
+++ plugins/ytmp4.js
@@ -1,16 +1,8 @@
-const { tiktok } = require('../lib/scrape')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
   if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command}`
   if (!args[0].match(/ytmp4/gi)) throw `*Link salah! Perintah ini untuk mengunduh video yt dengan link*\n\ncontoh:\n${usedPrefix + command}`
-
-  // tiktok(args[0]).then(async res => {
-  //   let tiktok = JSON.stringify(res)
-  //   let json = JSON.parse(tiktok)
-  //   // m.reply(require('util').format(json))
-  //   await conn.sendVideo(m.chat, json.nowm, '*© AriaBotz*', m, { thumbnail: await (await fetch(json.nowm)).buffer() })
-  // })
 
   let res = await fetch('http://ariarestapii.herokuapp.com/api/ytmp4?url=args[0]&apikey=aria')
   if (!res.ok) throw await `${res.status} ${res.message}`
