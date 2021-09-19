ba1f725addc50433a2186ab563ae846d5742860a
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-19 09:39:02 +0700

    Update faktaunik.js

--- plugins/faktaunik.js
+++ plugins/faktaunik.js
@@ -15,12 +15,12 @@ handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
-
+handler.register = true
 handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
 handler.exp = 0
-handler.limit = false
+handler.limit = true
 
 module.exports = handler
