99122e9b22b4c3c484ebbaf1d8bd49e9697d5e8c
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-19 12:59:27 +0700

    Create army.js

---
+++ plugins/army.js
@@ -0,0 +1,25 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args[0]) throw 'Masukkan Parameter Teks'
+  m.reply('Sedang Diproses...')
+  let res = `https://ariarestapii.herokuapp.com/api/oxy/army?text=${response}&apikey=aria`
+  conn.sendFile(m.chat, res, 'poly.mp4', `Ini Kak`, m, false)
+}
+handler.help = ['army'].map(v => v + ' <teks>')
+handler.tags = ['photooxy']
+
+handler.command = /^(army)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = true
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
