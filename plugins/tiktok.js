e79b8567bdb24a66d51574eb4015845e18377d08
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-19 21:39:08 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ')
   if (!args[0]) throw 'Masukkan Parameter'
   await conn.fakeReply(m.chat, '*[❗] Bntar ngab..*', '0@s.whatsapp.net', '*BOT By ZyxMapLe*', 'status@broadcast')
-  let res = await fetch(`https://ariarestapii.herokuapp.com/api/tiktokdl?url=${response}&apikey=aria`)
+  let res = await fetch(`https://ariarestapii.herokuapp.com/api/mediafire2?url=${response}&apikey=aria`)
   if (res.Status !== 200) throw await res.text()
   let json = await res.json()
   let video = json.result.WithWM
-  conn.sendFile(m.chat, video, `tiktok.mp4`, `Ini Kak`, m)
+  conn.sendVideo(m.chat, video, `tiktok.mp4`, `Ini Kak`, m)
 }
 
 handler.help = ['tiktokaudio'].map(v => v + ' <url>')
