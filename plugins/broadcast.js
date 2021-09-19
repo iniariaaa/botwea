dd76f0a181d1da306a1ca8692b8f1cdc5b16441a
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-19 14:26:30 +0700

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -1,17 +1,50 @@
-let handler = async (m, { conn, text }) => {
-  let chats = conn.chats.all().filter(v => !v.read_only && v.message && !v.archive).map(v => v.jid)
-  let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
+const { MessageType } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler = async (m, { conn, text, participants }) => {
+const ftoko = {
+key: {
+			fromMe: false,
+			participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "status@broadcast" } : {})
+		},
+		message: {
+			"productMessage": {
+				"product": {
+					"productImage":{
+						"mimetype": "image/jpeg",
+						"jpegThumbnail": fs.readFileSync('./src/cha.jpg'), //Gambarnye
+					},
+					"title": `BROADCAST`, //Kasih namalu 
+					"description": `Rp 50,000`, 
+					"currencyCode": "Rp",
+					"priceAmount1000": "500000",
+					"retailerId": `ppk`,
+					"productImageCount": 1
+				},
+				    "businessOwnerJid": `628162633549@s.whatsapp.net`
+		}
+	}
+}
+
+let users = participants.map(u => u.jid)
+  let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v => v.jid)
+  let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_\nestimasi selesai ${chats.length * 1.5} detik`, m)
-  for (let id of chats) {
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
+  for (let id of groups) {
     await delay(1500)
-    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 AriaBotz Broadcast 〕\n\n' + teks + '\n' + readMore + randomID(32)), true).catch(_ => _)
+    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks), 'conversation', { quoted: ftoko, contextInfo: { mentionedJid: users, externalAdReply :{
+mediaUrl: 'https://youtu.be/ppt6ZIW20ig',
+mediaType: 2,
+title: '',
+body: 'hehe',
+thumbnailUrl: 'https://telegra.ph/file/efd304268e9b183c9f8a3.jpg'
+}}}) // *Crewbot by @Galang*
   }
   m.reply('_*Broadcast Selesai*_')
 }
-handler.help = ['broadcast', 'bc'].map(v => v + ' <teks>')
+handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(broadcast|bc)$/i
+handler.command = /^(broadcast|bc)(group|grup|gc)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
@@ -28,6 +61,6 @@ module.exports = handler
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
-const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+const randomID = length => require('crypto').randomBytes(Math.ceil(length * .999)).toString('hex').slice(0, length)
 
 const delay = time => new Promise(res => setTimeout(res, time))
