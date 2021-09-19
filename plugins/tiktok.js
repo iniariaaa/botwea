ba0023b8746fedbb71b6cbea777fbfd6da585acc
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-19 21:47:30 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command}`
 
   let res = await fetch('https://ariarestapii.herokuapp.com/api/mediafire2?url=${response}&apikey=aria')
-  if (res.Status !== 200) throw await res.text()
+  if (res.status !== 200) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
   await m.reply(global.wait)
