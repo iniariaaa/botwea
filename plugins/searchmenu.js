cc0bb96de28f0810956944bc1a8fab01f5a4c150
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-20 13:52:41 +0700

    Create searchmenu.js

---
+++ plugins/searchmenu.js
@@ -0,0 +1,37 @@
+let PhoneNumber = require('awesome-phonenumber')
+let levelling = require('../lib/levelling')
+
+let handler = async (m, { conn, usedPrefix }) => {
+
+  let pp = './src/avatar_contact.png'
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  try {
+    pp = await conn.getProfilePicture(who)
+  } catch (e) {
+
+  } finally {
+    let str = `
+┌─── ⳹°❀❬ SEARCHING MENU ❭❀°
+│✎ ${usedPrefix}kusonime <query>
+│✎ ${usedPrefix}samehadaku <query>
+└────────────┈ ⳹ ❋ཻུ۪۪⸙
+`.trim()
+    let mentionedJid = [who]
+    conn.sendFile(m.chat, pp, 'lp.jpg', str, m, false, { contextInfo: { mentionedJid }})
+  }
+}
+handler.help = ['searchmenu']
+handler.tags = ['searching']
+handler.command = /^(searchmenu)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = true
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
