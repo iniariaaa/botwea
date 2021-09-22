633206d4833fa6c0fcaceaf8844dd7578369f4a0
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-22 12:00:18 +0700

    Update S.js

--- plugins/S.js
+++ plugins/S.js
@@ -4,9 +4,10 @@ let handler = async (m, { conn }) => {
 function kintil(list) {
   return list[Math.floor(list.length * Math.random())]
 }
-let img = kintil(['https://telegra.ph/file/0ee3b8764de7285d66724.jpg', 'https://telegra.ph/file/e36861de63ca790b899be.jpg'])
+let img = kintil(['https://telegra.ph/file/ed87bfeecd285253080be.jpg', 'https://telegra.ph/file/d03be530f4945bfeead24.jpg', 'https://telegra.ph/file/338d4b0b05bb3e0693925.jpg', 'https://telegra.ph/file/ad5b14b881bdb97215bb5.jpg'])
 
 conn.sendButtonLoc(m.chat, await (await cuk(img)).buffer(), '*Welcome To AriaBotz*\nSilahkan Klik Tombol Dibawah', 'ariabotz', 'Menu', '.menu')
+}
 
 handler.command = /^(menu11)$/i
 handler.owner = false
