257f6747463e17e7bf3cf4fbc8b3a040a694bc32
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-22 11:27:19 +0700

    Update S.js

--- plugins/S.js
+++ plugins/S.js
@@ -1,5 +1,6 @@
-let handler = async (m, { conn }) =>
 let fetch = require('node-fetch')
+let handler = async (m, { conn }) =>
+
 let img = 'https://telegra.ph/file/0ee3b8764de7285d66724.jpg'
 conn.send2ButtonLoc(m.chat, await (await fetch(${pickRandom(ramdomnya)})).buffer(), `
 Tes sekali lagi :v
